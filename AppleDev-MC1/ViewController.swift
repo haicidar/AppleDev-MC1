fca544ba4d1717a20c03e32af6529bb046a2f370
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-04-07 14:32:01 +0700

    Initial Commit

---
+++ AppleDev-MC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleDev-MC1
+//
+//  Created by Rahmat Zulfikri on 07/04/20.
+//  Copyright © 2020 Rahmat Zulfikri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
